43bba382d8dc266c6f3a59ad7af4647999bd420e
Patryk Krawczyk <patryk.krawczyk.it@gmail.com>
2023-01-02 19:31:59 +0100

    remove tasks, added class to style

--- js/script.js
+++ js/script.js
@@ -10,20 +10,6 @@
     },
   ];
 
-  const render = () => {
-    let htmlString = "";
-
-    for (const task of tasks) {
-      htmlString += `
-                <li ${task.done ? 'style="text-decoration: line-through"' : ""}>
-                    ${task.content}
-                </li>
-            `;
-    }
-
-    document.querySelector(".js-tasks").innerHTML = htmlString;
-  };
-
   const addNewTask = (newTaskContent) => {
     tasks.push({
       content: newTaskContent,
@@ -32,7 +18,36 @@
     render();
   };
 
-  onFormSubmit = (event) => {
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+                <li ${task.done ? 'style="text-decoration: line-through"' : ""}>
+                    <button class="form__submit--done js-done">Zrobione</button>
+                    ${task.content}
+                    <button class="form__submit--remove js-remove">Usuń</button>
+                </li>
+            `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButtons, index) => {
+      removeButtons.addEventListener("click", () => {
+        removeTask();
+      });
+    });
+  };
+
+  const onFormSubmit = (event) => {
     event.preventDefault();
 
     const newTaskContent = document.querySelector(".js-newTask").value.trim();
